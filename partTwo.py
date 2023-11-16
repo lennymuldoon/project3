472d158a78f1ff378782ff8ead032972020c8d0a
lennymuldoon <lenny.muldoon@gmail.com>
2023-11-16 12:20:42 -0600

    Edited function from + 153 to +1.53

--- partTwo.py
+++ partTwo.py
@@ -2,9 +2,10 @@
 #Leonard Muldoon
 #11/16/2023
 #--------------
+
 #func is the function used for part 2
 def func(x):
-    return ((.34*x**2) + (2.98*x) + 153)
+    return ((.34*x**2) + (2.98*x) + 1.53)
 
 #finds the integral of functon f starting from point a, to point b, and n is num of rectangles
 def integral(f, a, b, n):
@@ -17,7 +18,7 @@ def integral(f, a, b, n):
 
 #finds the integral within tolerance with num rectangles and returns the num rectangles and area.
 def integralWithTol(a, b, n, t):
-    n2 = n * 3
+    n2 = n * 2
     area = integral(func, a, b, n)
     areaPrime = integral(func, a, b, n2)
     while abs(area - areaPrime) >= t:
@@ -30,5 +31,5 @@ a = 2
 b = 4
 numRect = 10
 tol = 0.00001
-n, area = integralWithTol(a, b, numRect, tol)
-print("Area: ", area, " Number of rectangles: ", n)
+n, areaFinal = integralWithTol(a, b, numRect, tol)
+print("Area: ", areaFinal, " Number of rectangles: ", n)
