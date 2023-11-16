9b3fefbfd462607c44ae2d3e3b26ef8762e71032
lennymuldoon <lenny.muldoon@gmail.com>
2023-11-16 09:56:33 -0600

    edited and finalized part 1a report

--- partOne.py
+++ partOne.py
@@ -11,9 +11,9 @@ def derivative(f, x, h):
 def func(x):
     return ((4*x**3) + (2*x**2) - (3*x) + 5)
 
-#calc derivative at x=1
-x1 = 1
-deltaX = .0000000001
+#calc derivative at x=1 and -3 (swapped values when testing as needed)
+x1 = -3
+deltaX = 0.0000001
 derivativeOfX1 = derivative(func, x1, deltaX)
 
 print("Derivative for part 1a at x= ", x1, " with a delta X of: ", deltaX, " is: ", derivativeOfX1)
@@ -29,18 +29,36 @@ Run 1:
 Derivative for part 1a at x=  1  with a delta X of:  0.001  is:  13.014003999998636
 error is .01 which is not in my tolerance
 
+Derivative for part 1a at x=  -3  with a delta X of:  0.001  is:  92.96600399999022
+error is .004 which is not in my tolerance
+
 Run 2:
 Derivative for part 1a at x=  1  with a delta X of:  0.0001  is:  13.001400039982514
 error is .001 which is not in my tolerance
+
+Derivative for part 1a at x=  -3  with a delta X of:  0.0001  is:  92.99660004032262
+error is .0034 which is not in my tolerance
 
 Run 3:
 Derivative for part 1a at x=  1  with a delta X of:  1e-05 (.00001)  is:  13.00014000040761
 error is .0001 which is not in my tolerance
 
+Derivative for part 1a at x=  -3  with a delta X of:  1e-05 (.00001) is:  92.99966000071434
+error is .00034 which is not in my tolerance
+
 Run 4:
 Derivative for part 1a at x=  1  with a delta X of:  1e-06 (.000001) is:  13.000014000397186
 error is .00001 which is in my tolerance
 
+Derivative for part 1a at x=  -3  with a delta X of:  1e-06 (.000001) is:  92.99996602862848
+error is .000034 which is not in my tolerance
 
-I stopped here on run 6 because I have an error which is in my tolerance. A delta X of .00000001 is acceptable for x = 1
+Run 5:
+Derivative for part 1a at x=  1  with a delta X of:  1e-07 (.0000001) is:  13.000001413132622
+error is .000001 which is in my tolerance
+
+Derivative for part 1a at x=  -3  with a delta X of:  1e-07 (.0000001) is:  92.99999661038783
+error is .0000034 which is in my tolerance
+
+I stopped here on run 6 because I have an error which is in my tolerance. A delta X of .0000001 is acceptable for x = 1 and x = 3
 """
