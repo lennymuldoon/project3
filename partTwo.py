656ff1496d553444b4de56eb4e7f363700e4c200
lennymuldoon <lenny.muldoon@gmail.com>
2023-11-16 11:26:13 -0600

    created integral function

--- partTwo.py
+++ partTwo.py
@@ -2,3 +2,17 @@
 #Leonard Muldoon
 #11/16/2023
 #--------------
+#func is the function used for part 2
+def func(x):
+    return ((.34*x**2) + (2.98*x) + 153)
+
+#finds the integral of functon f starting from point a, to point b, and n is num of rectangles
+def integral(f, a, b, n):
+    deltaX = (b-a)/n
+    area = 0
+    for i in range(n):
+        height = f(a + i * deltaX)
+        area+= height * deltaX
+    return area
+
+print(integral(func, 0, 1, 20))
